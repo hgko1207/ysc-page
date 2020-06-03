496c2e0045641552791837b9aa82ae48a47e6131
hgko1207 <khkkhk1207@gmail.com>
2020-06-03 11:43:10 +0900

    fitst commit

---
+++ ysc-page/src/main/java/org/onsemiro/ysc/page/YscPageApplication.java
@@ -0,0 +1,13 @@
+package org.onsemiro.ysc.page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YscPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YscPageApplication.class, args);
+	}
+
+}
---
+++ ysc-page/src/test/java/org/onsemiro/ysc/page/YscPageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onsemiro.ysc.page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YscPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
