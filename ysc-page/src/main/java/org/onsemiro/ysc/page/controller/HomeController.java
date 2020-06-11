fa9c11728e3c0ab1952735fcab5fcaed0984e210
hgko1207 <khkkhk1207@gmail.com>
2020-06-11 14:52:12 +0900

    초기 화면 설정

---
+++ ysc-page/src/main/java/org/onsemiro/ysc/page/controller/HomeController.java
@@ -0,0 +1,18 @@
+package org.onsemiro.ysc.page.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+    public String index(Model model) {
+		 return "redirect:home";
+    }
+	
+	@GetMapping("home")
+    public void home(Model model) {
+    }
+}
